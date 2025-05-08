49d6c6fe08bd4e25382231278f9dd887a33856d8
marlediv <marco.grochowiak@outlook.com>
2025-05-08 15:05:00 +0200

    Angepasste frontend/src/pages/Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 const handleLogin = async (email, passwort) => {
   try {
-    const response = await apiRequest("/login", "POST", { email, passwort });
+    const response = await apiRequest("/api/auth/login", "POST", { email, passwort });
     alert("Login erfolgreich ✅");
   } catch (error) {
     alert("Login fehlgeschlagen ❌");
