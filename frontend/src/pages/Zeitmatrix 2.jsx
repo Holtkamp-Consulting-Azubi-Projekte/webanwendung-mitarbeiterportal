4b9c3664cf8b4b74711062122bb69381a2b4f7ef
marlediv <marco.grochowiak@outlook.com>
2025-06-02 15:52:29 +0200

    Start Entwicklung Feature Projekte-Seite

--- frontend/src/pages/Zeitmatrix 2.jsx
+++
@@ -1,8 +0,0 @@
-export default function Zeitmatrix() {
-    return (
-      <div className="p-8">
-        <h1 className="text-2xl font-bold text-primary">⌛️ Zeitmatrix</h1>
-      </div>
-    );
-  }
-  
